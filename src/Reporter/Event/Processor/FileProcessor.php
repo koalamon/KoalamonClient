87ca045be7fbca29965bbf03dee0daee2e41c0a0
Nils Langner <nils.langner@leankoala.com>
2021-02-07 15:54:40 +0100

    public static function for creating hash file names

--- src/Reporter/Event/Processor/FileProcessor.php
+++ src/Reporter/Event/Processor/FileProcessor.php
@@ -18,6 +18,9 @@ class FileProcessor implements Processor
         $this->publicHost = $publicHost;
     }
 
+    /*
+     * @todo the attributes could be stores within ONE json document and not many
+     */
     public function process(Event $event)
     {
         $attributes = [];
@@ -70,17 +73,17 @@ class FileProcessor implements Processor
 
     private function persistValue($key, $value, $timeToLiveInDays, Event $event)
     {
-        $date = date('m0d', strtotime('+ ' . $timeToLiveInDays . ' days'));
+        $date = date('m0d0H', strtotime('+ ' . $timeToLiveInDays . ' days'));
         $hash = $date . md5(json_encode($value));
 
-        $dir = $this->baseDir . '/' . substr($hash, 0, 2) . '/' . substr($hash, 3, 2) . '/';
+        $dir = self::getFilenameFromHash($this->baseDir, $hash);
 
         if (!file_exists($dir)) {
             echo "Creating directory: " . $dir;
             @mkdir($dir, 0777, true);
 
             if (!file_exists($dir)) {
-                echo "\nWARNING: Unable to create " . $dir . ". Attributes are not stored.\n";
+                echo "\nWARNING: Unable to create " . $dir . ". Attributes (hash: " . $hash . ") are not stored.\n";
                 return;
             }
         }
@@ -92,6 +95,26 @@ class FileProcessor implements Processor
         $this->memory[md5(serialize($value))] = $storageString;
 
         return $storageString;
+    }
+
+    /**
+     * Create a filename for the given hash.
+     *
+     * @param string $baseDir
+     * @param string $hash
+     * @param int $version
+     *
+     * @return string
+     */
+    public static function getFilenameFromHash($baseDir, $hash, $version = 2)
+    {
+        switch ($version) {
+            case 1:
+                return $baseDir . '/' . substr($hash, 0, 2) . '/' . substr($hash, 3, 2) . '/';
+            case 2:
+                return $baseDir . '/' . substr($hash, 0, 2) . '/' . substr($hash, 3, 2) . '/' . substr($hash, 6, 2) . '/';
+        }
+
     }
 
     static public function createByEnvironmentVars($baseDirectory)
